a5601bc375702c861c6d5956fccb16b6c5bf36c4
seangongz <sean.gongz@gmail.com>
2017-02-18 21:33:25 +0100

    refactor the naming of function and variables

--- smsutil.py
+++ smsutil.py
@@ -16,13 +16,10 @@ class SMS_CHECKER:
         inbox = '/var/spool/gammu/inbox/'
         files = os.listdir(inbox)
         files.sort()
-        absFile = []
 
-        # todo refactor with []
-        for f in files:
-            absFile.append(os.path.join(inbox, f))
+        abs_files = [os.path.join(inbox, f) for f in files]
 
-        self.inbox_files = absFile.copy()
+        self.inbox_files = abs_files.copy()
         self.inbox_files.sort()
 
     def _move_to_archieve(self, file):
@@ -47,7 +44,7 @@ class SMS_CHECKER:
         tmp_sms = dict()
         for f in self.inbox_files:
             tmp_sms['file'] = f
-            tmp_sms['text'] = getSMSFromFile(f)
+            tmp_sms['text'] = get_sms_from_file(f)
             self.sms.append(f)
 
         return self.sms
@@ -62,7 +59,7 @@ class SMS_CHECKER:
             self._move_to_archieve(f)
 
 
-def getSMSFromFile(file):
+def get_sms_from_file(file):
     # output:
     # 时间: 2015.10.10 09:01:33
     # 来自: 10010
@@ -72,7 +69,7 @@ def getSMSFromFile(file):
     encoding = 'utf-16'
     # get time stamp and sender
     try:
-        date_time, sender = getTimeSender(file)
+        date_time, sender = get_time_sender(file)
         sms = '时间: ' + date_time + '\n'
         sms = sms + '来自: ' + sender + '\n'
     except Exception as e:
@@ -88,7 +85,7 @@ def getSMSFromFile(file):
     return sms
 
 
-def getTimeSender(file):
+def get_time_sender(file):
     # 'IN20151010_090133_00_10010_00.txt' --> ('2015.10.10 09:01:33', '10010')
     timeSenderRegx = re.compile(r'IN(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})_\d{2}_(.*)_\d{2}')
     mo = timeSenderRegx.search(file)
@@ -101,7 +98,7 @@ def getTimeSender(file):
     return (date_time, sender)
 
 if __name__ == '__main__':
-    sms = SMS()
+    sms = SMS_CHECKER()
     all_sms = sms.get_sms()
     sms.archieve(all_sms)
 
