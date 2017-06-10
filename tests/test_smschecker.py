adbdcf2793e4e57a92b8a2662dbab9555f68005f
seangongz <sean.gongz@gmail.com>
2017-06-10 23:51:02 +0200

    add more test case for get_sms_from_file

--- tests/test_smschecker.py
+++ tests/test_smschecker.py
@@ -1,4 +1,6 @@
-from smschecker import get_time_sender
+from smschecker import get_time_sender, get_sms_from_file
+import shutil
+import os
 
 
 def test_get_time_sender():
@@ -6,3 +8,19 @@ def test_get_time_sender():
     time, sender = get_time_sender('IN20151010_090133_00_10010_00.txt')
     assert time == '2015.10.10 09:01:33'
     assert sender == '10010'
+
+
+def test_get_sms_from_file():
+    tmp_file = 'IN20151010_090133_00_10010_00.txt'
+    content = '消息'
+    with open(tmp_file, 'w',encoding='utf-16') as f:
+        f.write(content)
+
+    ret = get_sms_from_file(tmp_file)
+
+    time, sender = get_time_sender(tmp_file)
+
+    if os.path.exists(tmp_file):
+        os.remove(tmp_file)
+
+    assert ret == "时间: " + time + "\n" + "来自: " + sender + "\n" + content
