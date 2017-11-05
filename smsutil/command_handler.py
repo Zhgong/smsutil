b490f8ba698edcc8fd4b572b2c69d4406e31a76b
seangongz <sean.gongz@gmail.com>
2017-11-05 21:00:09 +0100

    create execute_cmd method

--- smsutil/command_handler.py
+++ smsutil/command_handler.py
@@ -16,29 +16,26 @@ class CommandMiddleWare:
         logging.info("%s: %s" % (self.telegram_update.message.text,msg))
         self.telegram_bot.sendMessage(chat_id=self.telegram_update.message.chat_id, text=msg)
 
-    def execute(self):
+    def execute_cmd(self):
+        msg = None # message to be returned
         msg_list = self.message.text.split(' ')
 
         msg_cmd = msg_list.pop(0).lower()
 
         if msg_cmd == 'name':
             msg = cmd.get_bot_name(self.telegram_bot, self.telegram_update)
-            self.send_message(msg)
 
         elif msg_cmd == 'status':
             msg = cmd.get_sms_process_info()
-            self.send_message(msg)
         elif msg_cmd == 'reboot':
             if not msg_list:
                 msg = "缺少参数. reboot pi?"
-                self.send_message(msg)
-                return
+                return msg
 
             par = msg_list.pop(0).lower()
             if not par == 'pi':
                 msg = "错误参数. reboot pi?"
-                self.send_message(msg)
-                return
+                return msg
 
             msg = "重启raspberry pi"
             self.send_message(msg)
@@ -47,12 +44,10 @@ class CommandMiddleWare:
             if not result == 0:
                 # 执行命令直接返回错误
                 msg = "重启raspberry pi失败"
-                self.send_message(msg)
-                return
+                return msg
             # 如果成功重启，一下命令不会被执行
             sleep(5)
             msg = "重启raspberry pi失败"
-            self.send_message(msg)
 
         elif msg_cmd == 'cmd':
             sys_cmd = " ".join(msg_list)
@@ -65,8 +60,11 @@ class CommandMiddleWare:
             else:
                 # empty command
                 msg = "Empty command"
-            self.send_message(msg)
 
         else:
             msg = cmd.get_help_msg()
+        return msg
+
+    def execute(self):
+            msg = self.execute_cmd()
             self.send_message(msg)
