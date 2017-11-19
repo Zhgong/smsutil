de339d9706cc916aa1e047d963a58b99189041b3
seangongz <sean.gongz@gmail.com>
2017-11-19 16:53:32 +0100

    create execute_cmd method

--- smsutil/command_handler.py
+++ smsutil/command_handler.py
@@ -8,6 +8,9 @@ class CommandMiddleWare:
         self.message = message
         self.telegram_bot = telegram_bot
         self.telegram_update = telegram_update
+        self.msg_list = None
+        self.cmd_dict = dict()
+        self.gen_cmd_list()
 
     def send_message(self, msg=None):
         if not msg:
@@ -18,49 +21,14 @@ class CommandMiddleWare:
 
     def execute_cmd(self):
         msg = None # message to be returned
-        msg_list = self.message.text.split(' ')
+        self.msg_list = self.message.text.split(' ')
 
-        msg_cmd = msg_list.pop(0).lower()
+        msg_cmd = self.msg_list.pop(0).lower()
 
-        if msg_cmd == 'name':
-            msg = cmd.get_bot_name(self.telegram_bot, self.telegram_update)
+        temp_cmd = self.cmd_dict.get(msg_cmd)
 
-        elif msg_cmd == 'status':
-            msg = cmd.get_sms_process_info()
-        elif msg_cmd == 'reboot':
-            if not msg_list:
-                msg = "缺少参数. reboot pi?"
-                return msg
-
-            par = msg_list.pop(0).lower()
-            if not par == 'pi':
-                msg = "错误参数. reboot pi?"
-                return msg
-
-            msg = "重启raspberry pi"
-            self.send_message(msg)
-            result =  cmd.reboot() # reboot machine
-
-            if not result == 0:
-                # 执行命令直接返回错误
-                msg = "重启raspberry pi失败"
-                return msg
-            # 如果成功重启，一下命令不会被执行
-            sleep(5)
-            msg = "重启raspberry pi失败"
-
-        elif msg_cmd == 'cmd':
-            sys_cmd = " ".join(msg_list)
-
-            if sys_cmd:
-                try:
-                    msg = cmd.exec_cmd(sys_cmd)
-                except Exception as e:
-                    msg = str(e)
-            else:
-                # empty command
-                msg = "Empty command"
-
+        if temp_cmd:
+            msg=temp_cmd()
         else:
             msg = cmd.get_help_msg()
         return msg
@@ -68,3 +36,54 @@ class CommandMiddleWare:
     def execute(self):
             msg = self.execute_cmd()
             self.send_message(msg)
+
+    def name_cmd(self):
+        msg = cmd.get_bot_name(self.telegram_bot)
+        return msg
+
+    def status_cmd(self):
+        msg = cmd.get_sms_process_info()
+        return msg
+
+    def reboot_cmd(self):
+        if not self.msg_list:
+            msg = "缺少参数. reboot pi?"
+            return msg
+
+        par = self.msg_list.pop(0).lower()
+        if not par == 'pi':
+            msg = "错误参数. reboot pi?"
+            return msg
+
+        msg = "重启raspberry pi"
+        self.send_message(msg)
+        result =  cmd.reboot() # reboot machine
+
+        if not result == 0:
+            # 执行命令直接返回错误
+            msg = "重启raspberry pi失败"
+            return msg
+        # 如果成功重启，一下命令不会被执行
+        sleep(5)
+        msg = "重启raspberry pi失败"
+        return msg
+
+    def cmd_cmd(self):
+        sys_cmd = " ".join(self.msg_list)
+
+        if sys_cmd:
+            try:
+                msg = cmd.exec_cmd(sys_cmd)
+            except Exception as e:
+                msg = str(e)
+        else:
+            # empty command
+            msg = "Empty command"
+
+        return msg
+
+    def gen_cmd_list(self):
+        self.cmd_dict['name'] = self.name_cmd
+        self.cmd_dict['status'] = self.status_cmd
+        self.cmd_dict['reboot'] = self.reboot_cmd
+        self.cmd_dict['cmd'] = self.cmd_cmd
